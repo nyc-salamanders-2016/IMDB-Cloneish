db335ad454626e4de9575c8aa1c747ca05d90d7f
Henri El Daher <henrieldaher@gmail.com>
2016-09-15 14:41:06 -0400

    complete addition of movie poster and info

--- app/assets/javascripts/components/app.es6.jsx
+++ app/assets/javascripts/components/app.es6.jsx
@@ -2,10 +2,10 @@ class App extends React.Component {
 	render(){
 		return(
 			<div>
-			<header>
-			  <label>Search for Stuff</label>
-	      <Header />
-			</header>
+				<header>
+				  <label>Search for Stuff!</label>
+		      <Header />
+				</header>
 
 				<section>
 			    <h3>Stuff to Watch with Edwin in the title</h3>
--- app/assets/javascripts/components/movie.es6.jsx
+++ app/assets/javascripts/components/movie.es6.jsx
@@ -2,9 +2,14 @@ class Movie extends React.Component{
 	render(){
 		// debugger
 		return (
-			<li className="movie-item">
-			  <span className="movie-name">{this.props.data.Title}</span>
-			</li>
+			<div>
+			  <img src={this.props.data.Poster} /><br/>
+				<span className="movie-name">{this.props.data.Title}</span>
+				<ul>
+					<li><span className="movie-item">{this.props.data.Year}</span></li>
+					<li><span className="movie-item">{this.props.data.Type}</span></li><br/>
+				</ul>
+			</div>
 		)
 	}
 }
